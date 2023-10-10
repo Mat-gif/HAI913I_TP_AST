217d74d34529a9d968459b95180752824dd84ae1
LOUM <98965342+mloum1@users.noreply.github.com>
2023-10-10 09:26:38 +0200

    visitor pour deux classes A et B

--- couplage.md
+++ couplage.md
@@ -1,6 +1,8 @@
 ## calcul de metrique de couplage
 
 couplage entre les classes et puis le diviser par le nombre de relation
+
+
 Api de visualisation de clustering
 
 Exercice 3 : optionnel spoon meilleur JDT qui se base sur les modeles
@@ -16,3 +18,12 @@ algorithme de clustering dans la these donnée dans le cours et algorithme de ge
 on regarde les deux partage par la classe A et B 
 une fonction coiplage qui prend en parametres les deux et en sortie doit tracer la méthode m partagé par les deux classes 
 
+
+
+
+
+le nombre d'appels des méthodes appartenant  à A  appelé dans B et le nombre de méthodes appartenant à B appelé dans A 
+
+/
+
+par le nombre total de relations  la somme des re
---
+++ src/main/java/visitor/ClassVisitorAB.java
@@ -0,0 +1,31 @@
+package visitor;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+public class ClassVisitorAB extends ASTVisitor {
+
+    private String A;
+    private String B;
+
+    public ClassVisitorAB(String A, String B) {
+        this.A = A;
+        this.B = B;
+    }
+
+    public boolean visit(TypeDeclaration node) {
+        if (!node.isInterface()) {
+            A = node.getName().getFullyQualifiedName();
+            B = node.getName().getFullyQualifiedName();
+        }
+        return super.visit(node);
+    }
+
+    public String getA() {
+        return A;
+    }
+
+    public String getB() {
+        return B;
+    }
+}
